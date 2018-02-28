9229766a2c98e5e2e2b688808d6973ad14f409a8
LoikoDaria <36641426+LoikoDaria@users.noreply.github.com>
2018-02-28 19:31:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  let xQuad=' * x^2 ';
+  let x=' * x ';
+  
+  let array=equation.split(xQuad);
+  let array2=array[1].split(x);
+  
+  let a=Number(array[0]);
+  let b=Number(array2[0].replace(/\s/g,''));
+  let c=Number(array2[1].replace(/\s/g,''));
+  
+  let D=(b*b)-4*a*c;
+  
+  let x1=Math.round((-b+Math.sqrt(D))/(2*a));
+  let x2=Math.round((-b-Math.sqrt(D))/(2*a));
+  
+  return result = [x1,x2].sort(function(a,b){
+		return a-b;
+  });;
+  
 }
